d377bd2838e432c2ac3b220fcce0b66da2450240
Kevin <kevinng521387@gmail.com>
2025-08-08 11:30:40 -0400

    Updates

---
+++ Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Importing the CSS file for styling
+import 'reactjs-popup/dist/index.css';
+import App from '../src/App';
+
+function Timeline() {
+    return (
+    <div className="Timeline">
+          <title>Team SOAR</title>
+          <h1>Team SOAR</h1>
+          <h2>Timeline</h2>
+          </div>
+    );
+}
+
+export default App;
